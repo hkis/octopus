12bcd7bea838e45dec78c6397bdd0fd0236893e4
binggege <1204363612@qq.com>
2013-11-01 09:55:21 +0800

    添加注释
    说明接口参数修改方法

--- PDFReader/html/js/pdfReader.js
+++ PDFReader/html/js/pdfReader.js
@@ -3,9 +3,9 @@
         var Pdf = new Bing.Pdf({
             uid:'BingGeGe',
             allowTypes:'pdf',
-            uploadUrl:'upload.php',
-            listUrl:'list.php',
-            loadImgUrl:'loadImg.php',
+            uploadUrl:'upload.php',//上传动态文件路径
+            listUrl:'list.php',//类表页读取路径
+            loadImgUrl:'loadImg.php',//主页面图片读取路径
             parent:$('body'),
             className:'pdf-main-body'
         });
