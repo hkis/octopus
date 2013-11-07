d3d29b06cc5813c50b19cce5bc4abd30053ace15
icexia <lingjiexia@126.com>
2013-11-07 11:34:05 +0800

    daf

--- PDFReader/UIInterface/uplod.aspx
+++
@@ -1,1 +0,0 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="uplod.aspx.cs" Inherits="UIInterface_uplod" %>
--- PDFReader/html/upload.php
+++
@@ -1,5 +0,0 @@
-<?php
-$file = $_FILES["FileData"];
-$returnVal = "{\"title\":\"".$file["name"]."\",\"author\":\"".$_POST['Uid']."\",\"time\":\"".date("Y-m-d")."\"}";
- echo '<script type="text/javascript">window.parent.Bing.templateData=\''.$returnVal.'\'</script>';
-?>
